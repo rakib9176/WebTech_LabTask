d63b0d48d8910b878931a3f0ddff619540ed223f
rakib9176 <rakibshajib06@gmail.com>
2021-03-16 18:38:03 +0600

    Updated

---
+++ registration lab 1 19-39825-1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registration From</title>
+<h3>Registration</h3>
+</head>
+
+<body>
+        <form align="center">
+        Name:
+        <input type="text" name="Name"><br><br>
+        Email:
+        <input type="text" name="Email"><br><br>
+        UserName:
+        <input type="text" name="UserName"><br><br>
+        Password:
+        <input type="text" name="Password"><br><br>
+        Confirm Password:
+        <input type="text" name="Confirm Password"><br><br>
+        Gender:<br>
+        <input type="radio" name="gender" value="Male">
+        <input type="radio" name="gender" value="Female">
+        <input type="radio" name="gender" value="Other">
+
+        Date of Birth:
+        <input type="text" name="Date of Birth"><br><br>
+
+        <input type="submit" value="Submit">
+
+
+
+
+        </form>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
